db146969ec0bab917197baadc007fc9431b59ae8
heuvil <heuler@outlook.com>
2019-08-30 21:13:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-project8-w3wsuv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-project8-w3wsuv)
---
+++ src/app/cart.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class CartService {
+  items = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  delFromCart(product) {
+    var index = this.items.indexOf(product);
+    this.items.splice(index, 1);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+}
